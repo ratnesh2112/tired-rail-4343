e73642e5c32a96ad79a18e48d128256a5722d65d
Shravankumar1234 <99716701+Shravankumar1234@users.noreply.github.com>
2022-07-20 20:13:37 +0530

    day2

--- kindmeal-my-clone/src/App.js
+++ kindmeal-my-clone/src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { SignupButtonBox } from './Pages/SignupButtonBox';
+import {Routes,Route} from "react-router-dom"
+import { ForgetPassword } from './Pages/ForgetPassword';
+import { Signup } from './Pages/Signup';
 
 function App() {
   return (
     <div className="App">
-      
+      <SignupButtonBox/>
+      <Routes>
+        <Route path="/Signup" element={<Signup/>}></Route>
+      </Routes>
+
     </div>
   );
 }
