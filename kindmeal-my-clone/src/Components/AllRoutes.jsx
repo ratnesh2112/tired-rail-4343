74e8d75e21804c5da0221e31ce269a029e89c2d5
ratnesh2112 <mohitrai2112@gmail.com>
2022-07-22 08:07:41 +0530

    Updated index and app

--- kindmeal-my-clone/src/Components/AllRoutes.jsx
+++ kindmeal-my-clone/src/Components/AllRoutes.jsx
@@ -5,6 +5,7 @@ import Navbar from './Navbar'
 import TopSection from './TopSection'
 import Recipe from '../Pages/Recipe'
 import Footer from './Footer'
+import { MealDeals } from '../Pages/MealDeals'
 
 const AllRoutes = () => {
   return (
@@ -14,7 +15,7 @@ const AllRoutes = () => {
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="recipe" element={ <Recipe /> } />
-            <Route path="" element={""} />
+            <Route path="mealdeal" element={<MealDeals />} />
             <Route path="" element={""} />
             <Route path="" element={""} />
             <Route path="" element={""} />
