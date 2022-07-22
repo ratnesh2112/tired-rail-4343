3ff377f8ed4eb407500b1e3f48a586a7fc98db66
ratnesh2112 <mohitrai2112@gmail.com>
2022-07-22 07:51:06 +0530

    Updated Home Page

--- kindmeal-my-clone/src/App.js
+++ kindmeal-my-clone/src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import AllRoutes from './Components/AllRoutes';
+// import { MealDealsAbout } from './Components/MealDeals/MealDealsAbout/MealDealsAbout' 
+// import {BottomPoster} from './Components/MealDeals/MealDealsPosters/BottomPoster'
+// import {TopPoster} from './Components/MealDeals/MealDealsPosters/TopPoster'
+// import {MealDealsNavigation} from './Components/MealDeals/Navigation/MealDealsNavigation'
 
 function App() {
   return (
