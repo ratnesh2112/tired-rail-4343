ceb0db419e79533842c2481c2682ec9ce84fd69d
Shivamyadav07 <shivamvnsindia@gmail.com>
2022-07-22 20:02:11 +0530

    compleated the component part

--- kindmeal-my-clone/src/Components/MealDeals/MealDealsProductComponents/ProductComponents.jsx
+++ kindmeal-my-clone/src/Components/MealDeals/MealDealsProductComponents/ProductComponents.jsx
@@ -1,21 +1,17 @@
 import React from 'react'
 import styles from './ProductComponents.module.css'
 
-export const ProductComponents = () => {
+export const ProductComponents = ({data}) => {
     return (
         <div className={styles.maindiv}>
             <div className={styles.imagediv}>
-                <img className={styles.image} src='https://www.kindmeal.my/photos/deal/5/590-3137-l.jpg' alt='productImage' />
-                <p className={styles.heading}>shdgivam</p>
+                <img className={styles.image} src={data.imageUrl} alt='productImage' />
+                <p className={styles.heading}>{data.foodName}</p>
             </div>
             <div className={styles.infodiv}>
-                <p className={styles.dheading}>Rawsome - </p>
-                <p className={styles.dsubheading}> Sungai Buloh, Selangor</p>
-                <p className={styles.description}>
-                    Enjoy healthy, guilt-free, raw food meals artfully crafted for
-                    a nutritious and delicious experience.
-                    Indulge in a satisfying mea..
-                </p>
+                <p className={styles.dheading}>{data.cafeName} - </p>
+                <p className={styles.dsubheading}>{data.cafeLocation}</p>
+                <p className={styles.description}>{data.description1}</p>
                 <div className={styles.coupondiv}>
                     <button className={styles.button}>Get Free Coupon</button>
                     <div className={styles.stardiv}>
@@ -26,7 +22,7 @@ export const ProductComponents = () => {
                             <img className={styles.starimage} src='https://www.kindmeal.my/images/icon_star.png' alt='star' />
                             <img className={styles.starimage} src='https://www.kindmeal.my/images/icon_star.png' alt='star' />
                         </div>
-                        <p style={{ "marginTop": "7px", "marginLeft": "10px" }}>(6)</p>
+                        <p style={{ "marginTop": "7px", "marginLeft": "10px" }}>({data.rating})</p>
                     </div>
                 </div>
             </div>
@@ -38,11 +34,11 @@ export const ProductComponents = () => {
                 </div>
                 <div className={styles.discount}>
                     <p className={styles.discountp1}>KindMeal Discount</p>
-                    <p className={styles.discountp2}>20% Off</p>
+                    <p className={styles.discountp2}>{data.discount}</p>
                 </div>
                 <div className={styles.expire}>
                     <p className={styles.discountp1}>Expires In</p>
-                    <p className={styles.expirep2}>7 Days</p>
+                    <p className={styles.expirep2}>{data.expire}</p>
                 </div>
             </div>
         </div>
--- kindmeal-my-clone/src/Pages/MealDeals.jsx
+++ kindmeal-my-clone/src/Pages/MealDeals.jsx
@@ -5,15 +5,37 @@ import { TopPoster } from '../Components/MealDeals/MealDealsPosters/TopPoster'
 import { MealDealsNavigation } from '../Components/MealDeals/Navigation/MealDealsNavigation'
 import { BottomPoster } from '../Components/MealDeals/MealDealsPosters/BottomPoster'
 import { ProductComponents } from '../Components/MealDeals/MealDealsProductComponents/ProductComponents'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 export const MealDeals = () => {
+    const [data, setdata] = useState([]);
+
+    useEffect(() => {
+        axios.get(" http://localhost:8080/MealDealsData").then((res) => {
+            setdata(res.data);
+            console.log(res.data)
+        })
+    }, [])
+    // function getdata(){
+
+    // }
+
     return (
         <div>
             <MealDealsAbout />
             <div className={styles.maindiv}>
                 <TopPoster />
                 <MealDealsNavigation />
-                <ProductComponents/>
+                <div className={styles.componentdiv}>
+                    {data.map(e => {
+                        return (
+                            <ProductComponents data={e} />
+                        )
+                    })}
+                </div>
+
                 <MealDealsNavigation />
             </div>
             <BottomPoster />
